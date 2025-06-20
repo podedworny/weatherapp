3feaa9dfab0abd1fe4bb3d668793b9d894b345fa
Paweł Podedworny <p.poded@gmail.com>
2025-06-20 04:39:09 +0200

    cors

---
+++ src/main/java/com/weather/weatherapp/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.weather.weatherapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://nextjs-static-m5rv.onrender.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
